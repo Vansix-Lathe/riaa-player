dddaa48d725d7fb3f39b3807ca55125d1a518c91
Vansix-Lathe <vansix.store@gmail.com>
2026-06-22 15:10:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,5 @@
-const CACHE_NAME = 'riaa-player-v2';
-const urlsToCache = ['./index.html', './manifest.json', './icon.png'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).catch(()=>{})
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(names => Promise.all(
-      names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
-    ))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
+const CACHE_NAME = 'riaa-player-v3';
+const urlsToCache = ['./index.html','./manifest.json','./icon.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
